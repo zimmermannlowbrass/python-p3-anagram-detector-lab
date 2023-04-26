1da8f16c6591fc7fa4e03c4bfc2fe5a03853b40f
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-04-26 11:34:26 -0500

    completed labwork

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+class Anagram:
+    
+    def __init__(self, word):
+        self.word = sorted(word)
+
+    def match(self, l):
+        matchedList = []
+        for x in l:
+            if sorted(x) == self.word:
+                matchedList.append(x)
+        return matchedList
